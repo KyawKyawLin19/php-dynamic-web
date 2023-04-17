af5f10369fb26795189d86af4d698f03792a8b9d
kyawkyawlin <kkyawlin.connetis@gmail.com>
2023-04-17 20:36:31 +0630

    initial commit

---
+++ basic.php
@@ -0,0 +1,40 @@
+<?php
+
+    $books = [
+        [
+            'name' => 'Do Androids Dream of Electric Sheep',
+            'author' => 'Philip K. Dick',
+            'releaseYear' => 1968,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'Project Hail Mary',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2021,
+            'purchaseUrl' => 'http://example.com'
+        ],
+        [
+            'name' => 'The Martian',
+            'author' => 'Andy Weir',
+            'releaseYear' => 2011,
+            'purchaseUrl' => 'http://example.com'
+        ],
+    ];
+
+    function filter($items, $fn)
+    {
+        $filteredItems = [];
+
+        foreach($items as $item) {
+            if($fn($item)) {
+                $filteredItems[] = $item;
+            }
+        }
+        return $filteredItems;
+    }
+
+    $filteredItems = filter($books, function($book){
+        return $book['releaseYear'] > '1950';
+    });
+
+    require "index.view.php";
