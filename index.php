bd618a6b1e098014f97c0bf1b9e862f304119fd8
kyawkyawlin <kyawkyawlin19954@gmail.com>
2023-04-26 23:30:00 +0630

    sql injection

--- index.php
+++ index.php
@@ -7,7 +7,12 @@ $config = require('config.php');
 
 $db = new Database($config['database']);
 
-$posts = $db->query("select * from posts")->fetchAll();
+$id = $_GET['id'];
+
+//keyed wild card parameters
+$query = "select * from posts where id = :id";
+
+$posts = $db->query($query, [':id' => $id])->fetch();
 
 foreach($posts as $post) {
     echo "<li>" . $post['title'] . "</li>";
